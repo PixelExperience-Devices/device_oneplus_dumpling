92e89eaa9804c61b68933cf76170dfa475a20a7f
Volodymyr Zhdanov <wight554@gmail.com>
2019-05-08 06:54:51 +0000

    dumpling: set PRODUCT_AAPT_PREBUILT_DPI
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- device.mk
+++ device.mk
@@ -29,7 +29,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2160
