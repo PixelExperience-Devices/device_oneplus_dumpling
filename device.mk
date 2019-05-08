dd01ba9cace23260f37288408d018b7ffa09539d
Volodymyr Zhdanov <wight554@gmail.com>
2019-05-08 06:57:28 +0000

    dumpling: set aapt perf config
    * bonito (p3axl) has same screen params and uses 400dpi
    
    Change-Id: Ife53ecc3b2ed5f981d23c2a6cba42aaa1d69fb90
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- device.mk
+++ device.mk
@@ -29,6 +29,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := 400dpi
 PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 
 # Boot animation
