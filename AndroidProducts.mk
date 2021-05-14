4371b0b74d2292c8daa2ac02a1e952341f04ff6a
xLexip <x@lexip.dev>
2021-05-14 15:25:29 +0200

    dumpling: Adapt to AOSP
    * Lineage >> AOSP
    * Remove custom build fingerprint and description

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dumpling.mk
+    $(LOCAL_DIR)/aosp_dumpling.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_dumpling-user \
-    lineage_dumpling-userdebug \
-    lineage_dumpling-eng
+    aosp_dumpling-user \
+    aosp_dumpling-userdebug \
+    aosp_dumpling-eng
